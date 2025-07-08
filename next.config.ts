3c15bcdb7dbf164baf1555f06c5bc361390e502c
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2025-07-08 23:52:48 +0600

    remove validation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // ✅ this ignores TS errors at build
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
